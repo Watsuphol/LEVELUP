aaeaaab0a87aad845ffef6b6b7a975554fe09a99
Watsuphol <wasumc.design@gmail.com>
2024-06-08 11:40:47 +0700

    Updated

--- react_begin/src/App.js
+++ react_begin/src/App.js
@@ -1,12 +1,23 @@
-import Transaction from "./component/Transaction";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1 style={{ color: "red", textAlign: "center" }}>
-        แอพบัญชีรายรับ - รายจ่าย
-      </h1>
-      <Transaction />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- react_begin/src/index.js
+++ react_begin/src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// การสร้าง Component
+/* function HelloComponent() {
+  return <h1>Hello</h1>;
+} */
+
+// การสร้าง Class Component
+class HelloComponent2 extends React.Component {
+  return() {
+    <h1>Hello HelloComponent2</h1>;
+  }
+}
+
+ReactDOM.render(<HelloComponent2 />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
