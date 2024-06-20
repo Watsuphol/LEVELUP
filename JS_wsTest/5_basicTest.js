bd12d48ad27543790d695d6a0b069022605babed
Watsuphol <wasumc.design@gmail.com>
2024-06-20 23:14:55 +0700

    Updated

---
+++ JS_wsTest/5_basicTest.js
@@ -0,0 +1,25 @@
+//  "abcd"
+// [1], [2], [3], [4]
+// "a", "b", "c", "d"
+
+// ["a","b","c","d"]
+// loop ->
+
+function textNumber(str) {
+  let len = str.length;
+  let newStr = "";
+
+  for (let i = 0; i < len; i++) {
+    console.log(i);
+    if (i !== 0) {
+      newStr += "-";
+    }
+    for (j = 0; j <= i; j++) {
+      newStr += str[i];
+    }
+    console.log(newStr);
+  }
+  return newStr;
+}
+
+console.log(textNumber("big"));
