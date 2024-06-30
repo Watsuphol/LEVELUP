4630188d2d0dad5461227f7f237346b6b13f01d6
Watsuphol <wasumc.design@gmail.com>
2024-06-30 21:11:04 +0700

    Updated

---
+++ nodejs-jsd/1-local-module/app.js
@@ -0,0 +1,9 @@
+// entry point
+// 1 file = 1 module
+require("./math.js");
+
+const firstName = require('./math.js')
+
+console.log(firstName);
+
+
---
+++ nodejs-jsd/1-local-module/math.js
@@ -0,0 +1,3 @@
+const firstName = "test";
+
+module.exports = firstName;
