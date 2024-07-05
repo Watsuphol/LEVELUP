6b8692e2fbb69a5dadd78ac1f8cc580351ddcbf9
Watsuphol <wasumc.design@gmail.com>
2024-07-05 20:45:28 +0700

    Updated Assignment 10_interviewTest

--- JS_wsTest/10_interviewTest.js
+++ JS_wsTest/10_interviewTest.js
@@ -1,15 +1,41 @@
 function bossBaby(input) {
   console.log(input);
 
+  // create arr R and S
+  let arrR = [];
+  let arrS = [];
+
+  // For str for added to emtry array
   for (const str of input) {
     console.log(str);
-    if (input[0] === "R") {
+
+    if (str === "R") {
+      arrR.push(str);
+      console.log(arrR);
+      console.log(arrR.length);
+    }
+    if (str === "S") {
+      arrS.push(str);
+      console.log(arrS);
+    }
+  }
+
+  // Divice Solution
+  if (input[0] === "R") {
+    return "Bad boys";
+  }
+  if (input[0] === "S") {
+    if (arrS.length == arrR.length) {
       return "Bad boys";
     }
-    return "Good boys";
+    if (arrR.length > arrS.length) {
+      return "Good boys";
+    }
   }
 }
 
 console.log(bossBaby("SRSSRRR"));
 console.log(bossBaby("RSSRR"));
 console.log(bossBaby("SSSRRRRS"));
+console.log(bossBaby("SRRSSR"));
+console.log(bossBaby("SSRSRRR"));
